ef78a4bf6b621e91e8743095f7f551e79d90440f
akiharu12292003 <akiharu001229@gmail.com>
2023-11-17 14:12:58 +0900

    ログインページ

--- MyGitProject2c/src/main/java/com/example/demo/controller/gitLoginController.java
+++ MyGitProject2c/src/main/java/com/example/demo/controller/gitLoginController.java
@@ -1,5 +1,30 @@
 package com.example.demo.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
 public class gitLoginController {
 
+    @RequestMapping(path = "/gitpralogin", method = RequestMethod.GET)
+    public String showLoginForm() {
+        return "gitlogin";
+    }
+
+    @RequestMapping(path = "/gitpralogin", method = RequestMethod.POST)
+    public String processLogin(Model model, @RequestParam String id, @RequestParam String ps) {
+        // Dummy authentication check
+        if ("gitlogin".equals(id) && "gitpw".equals(ps)) {
+            // Authentication successful, perform appropriate actions (e.g., display login success message)
+            model.addAttribute("message", "ログイン成功");
+            return "gititemedit"; // Redirect to gititemedit.html on successful login
+        } else {
+            // Authentication failed, display error message
+            model.addAttribute("error", "IDまたはパスワードが正しくありません");
+            return "gitlogin";
+        }
+    }
 }
